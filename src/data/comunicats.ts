6d2e7c8a1443c9a31aaa955553e2a1cc4b0ba470
IAN RAMIREZ FERNANDEZ <ian.ramirez1@volkswagen-groupservices.com>
2026-05-07 10:41:20 +0200

    add socis news

---
+++ src/data/comunicats.ts
@@ -0,0 +1,27 @@
+export const comunicats = [
+  {
+    id: 1,
+    data: "2026-05-05",
+    titol: "📢 Casal d'estiu CV Martorell",
+    cos: "Benvolgudes famílies,\n\nUs informem que aquest estiu el club organitza el casal d'estiu del 29 de juny al 24 de juliol.\n\nPer conèixer el funcionament, activitats, horaris i fer la inscripció, cal accedir al formulari adjunt.\n\nEl full d'inscripció i el certificat de salut es poden descarregar des del mateix formulari i entregar-los a l'oficina o adjuntar-los on indica 'afegir arxiu'.\n\nPer qualsevol dubte, estem a la vostra disposició.\n\nGràcies!\nJunta CV Martorell",
+    formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfxq82wpf1tF8NLqa2uTfMTJJUu6unsYV-WxdhwBN60SIfEvw/viewform",
+    formulariLabel: "Formulari d'inscripció",
+  },
+  {
+    id: 2,
+    data: "2026-04-30",
+    titol: "📢 Formulari de preinscripció 26-27",
+    cos: "Us enviem el formulari de preinscripció per a la temporada 26-27 (també el rebreu per mail). Només cal omplir-ho una vegada.\n\nTeniu com a la data límit, per enviar-ho del 30 de juny. Us agrairem que el feu dins del termini, o el més aviat possible si ho teniu clar, per poder organitzar la temporada vinent.\n\nMoltes gràcies! 🙌",
+    formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfbgN7OpMD3cndujoOMAdnqMNSOo5G2oOqa1DuiVlTk9ZMfAw/viewform",
+    formulariLabel: "Formulari de preinscripció",
+  },
+  {
+    id: 3,
+    data: "2026-04-29",
+    titol: "🏐 Noves samarretes d’estiu del Club Volei Martorell!",
+    cos: "Ens fa molta il·lusió anunciar que ja estan a la venda! 🙌\n\nL’objectiu és omplir la grada de color, energia i suport als partits 💙💛 Ens encantaria veure totes les famílies animant juntes i lluint els colors del club!\n\n👉 Com fer la comanda?\nOmpliu el formulari amb les vostres dades i talla:\n\n💰 El pagament es farà presencialment al despatx nº3 i en efectiu.\nEl preu de la samarreta és de 12€\n\n👕 Si teniu dubtes amb la talla, podeu passar en horari d’oficina:\n\n* Dilluns: 19h a 20:30h\n* Divendres: 19h a 21h\n\nHi ha diferents talles disponibles per a tothom!\n\nEl vostre suport és clau per seguir creixent com a club i comunitat 💪\n\nMoltes gràcies per formar-ne part! 🙏",
+    formulari: "https://forms.gle/MfYhvdHvXk2EnuEj8",
+    formulariLabel: "Formulari samarretes",
+  }
+  // Afegeix aquí els propers comunicats
+];
