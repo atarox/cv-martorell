27b00fca4ab560b587e9cf9d1b4e7b47e4cec477
IAN RAMIREZ FERNANDEZ <ian.ramirez1@volkswagen-groupservices.com>
2026-05-13 11:18:16 +0200

    fix encoding comunicats

--- src/data/comunicats.ts
+++ src/data/comunicats.ts
@@ -1,36 +1,35 @@
 export const comunicats = [
-{
+  {
     id: 1,
     data: "2026-05-13",
-    titol: "馃摙 Casal tecnificació",
+    titol: "?? Casal tecnificació",
     cos: "Benvolgudes famílies,\n\nUs informem que aquest estiu, a banda del casal d'estiu habitual, també organitzarem un Casal de Tecnificació durant el mes de juliol.\n\nAquest casal es realitzarà de dilluns a dijous de 18h a 20h i estarà orientat a oferir un treball tècnic més específic i personalitzat, adaptat a les necessitats i evolució dels participants.\n\nLes setmanes de tecnificació estaran subjectes a un nombre mínim i màxim d'inscripcions, ja que volem garantir una atenció més individualitzada i una millor qualitat del treball realitzat. Per aquest motiu, les places seran limitades.\n\nPer a més informació, podeu accedir al formulari adjunt clicant sobre la paraula formulari.",
     formulari: "https://forms.gle/DzweQnFUBMWbCVGu5",
-    formulariLabel: "Formulari d'inscripci贸",
+    formulariLabel: "Formulari d'inscripció",
   },
-
   {
     id: 2,
     data: "2026-05-05",
-    titol: "馃摙 Casal d'estiu CV Martorell",
-    cos: "Benvolgudes fam铆lies,\n\nUs informem que aquest estiu el club organitza el casal d'estiu del 29 de juny al 24 de juliol.\n\nPer con猫ixer el funcionament, activitats, horaris i fer la inscripci贸, cal accedir al formulari adjunt.\n\nEl full d'inscripci贸 i el certificat de salut es poden descarregar des del mateix formulari i entregar-los a l'oficina o adjuntar-los on indica 'afegir arxiu'.\n\nPer qualsevol dubte, estem a la vostra disposici贸.\n\nGr脿cies!\nJunta CV Martorell",
+    titol: "?? Casal d'estiu CV Martorell",
+    cos: "Benvolgudes famílies,\n\nUs informem que aquest estiu el club organitza el casal d'estiu del 29 de juny al 24 de juliol.\n\nPer conèixer el funcionament, activitats, horaris i fer la inscripció, cal accedir al formulari adjunt.\n\nEl full d'inscripció i el certificat de salut es poden descarregar des del mateix formulari i entregar-los a l'oficina o adjuntar-los on indica 'afegir arxiu'.\n\nPer qualsevol dubte, estem a la vostra disposició.\n\nGràcies!\nJunta CV Martorell",
     formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfxq82wpf1tF8NLqa2uTfMTJJUu6unsYV-WxdhwBN60SIfEvw/viewform",
-    formulariLabel: "Formulari d'inscripci贸",
+    formulariLabel: "Formulari d'inscripció",
   },
   {
     id: 3,
     data: "2026-04-30",
-    titol: "馃摙 Formulari de preinscripci贸 26-27",
-    cos: "Us enviem el formulari de preinscripci贸 per a la temporada 26-27 (tamb茅 el rebreu per mail). Nom茅s cal omplir-ho una vegada.\n\nTeniu com a la data l铆mit, per enviar-ho del 30 de juny. Us agrairem que el feu dins del termini, o el m茅s aviat possible si ho teniu clar, per poder organitzar la temporada vinent.\n\nMoltes gr脿cies! 馃檶",
+    titol: "?? Formulari de preinscripció 26-27",
+    cos: "Us enviem el formulari de preinscripció per a la temporada 26-27 (també el rebreu per mail). Només cal omplir-lo una vegada.\n\nTeniu com a data límit per enviar-ho el 30 de juny. Us agrairem que el feu dins del termini, o el més aviat possible si ho teniu clar, per poder organitzar la temporada vinent.\n\nMoltes gràcies! ??",
     formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfbgN7OpMD3cndujoOMAdnqMNSOo5G2oOqa1DuiVlTk9ZMfAw/viewform",
-    formulariLabel: "Formulari de preinscripci贸",
+    formulariLabel: "Formulari de preinscripció",
   },
   {
     id: 4,
     data: "2026-04-29",
-    titol: "馃彁 Noves samarretes d鈥檈stiu del Club Volei Martorell!",
-    cos: "Ens fa molta il路lusi贸 anunciar que ja estan a la venda! 馃檶\n\nL鈥檕bjectiu 茅s omplir la grada de color, energia i suport als partits 馃挋馃挍 Ens encantaria veure totes les fam铆lies animant juntes i lluint els colors del club!\n\n馃憠 Com fer la comanda?\nOmpliu el formulari amb les vostres dades i talla:\n\n馃挵 El pagament es far脿 presencialment al despatx n潞3 i en efectiu.\nEl preu de la samarreta 茅s de 12鈧琝n\n馃憰 Si teniu dubtes amb la talla, podeu passar en horari d鈥檕ficina:\n\n* Dilluns: 19h a 20:30h\n* Divendres: 19h a 21h\n\nHi ha diferents talles disponibles per a tothom!\n\nEl vostre suport 茅s clau per seguir creixent com a club i comunitat 馃挭\n\nMoltes gr脿cies per formar-ne part! 馃檹",
+    titol: "??? Noves samarretes d’estiu del Club Volei Martorell!",
+    cos: "Ens fa molta il·lusió anunciar que ja estan a la venda! ??\n\nL’objectiu és omplir la grada de color, energia i suport als partits ???? Ens encantaria veure totes les famílies animant juntes i lluint els colors del club!\n\n?? Com fer la comanda?\nOmpliu el formulari amb les vostres dades i talla.\n\n?? El pagament es farà presencialment al despatx núm. 3 i en efectiu.\nEl preu de la samarreta és de 12�.\n\n?? Si teniu dubtes amb la talla, podeu passar en horari d’oficina:\n\n- Dilluns: 19h a 20:30h\n- Divendres: 19h a 21h\n\nHi ha diferents talles disponibles per a tothom!\n\nEl vostre suport és clau per seguir creixent com a club i comunitat ??\n\nMoltes gràcies per formar-ne part! ??",
     formulari: "https://forms.gle/MfYhvdHvXk2EnuEj8",
     formulariLabel: "Formulari samarretes",
   }
-  // Afegeix aqu铆 els propers comunicats
+  // Afegeix aquí els propers comunicats
 ];
