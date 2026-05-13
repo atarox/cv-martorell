2e7375b162be86fa662beb9456465fbfc938d53e
IAN RAMIREZ FERNANDEZ <ian.ramirez1@volkswagen-groupservices.com>
2026-05-13 11:27:15 +0200

    fix

--- src/data/comunicats.ts
+++ src/data/comunicats.ts
@@ -2,34 +2,34 @@ export const comunicats = [
   {
     id: 1,
     data: "2026-05-13",
-    titol: "?? Casal tecnificaci¨®",
-    cos: "Benvolgudes fam¨ªlies,\n\nUs informem que aquest estiu, a banda del casal d'estiu habitual, tamb¨¦ organitzarem un Casal de Tecnificaci¨® durant el mes de juliol.\n\nAquest casal es realitzar¨¤ de dilluns a dijous de 18h a 20h i estar¨¤ orientat a oferir un treball t¨¨cnic m¨¦s espec¨ªfic i personalitzat, adaptat a les necessitats i evoluci¨® dels participants.\n\nLes setmanes de tecnificaci¨® estaran subjectes a un nombre m¨ªnim i m¨¤xim d'inscripcions, ja que volem garantir una atenci¨® m¨¦s individualitzada i una millor qualitat del treball realitzat. Per aquest motiu, les places seran limitades.\n\nPer a m¨¦s informaci¨®, podeu accedir al formulari adjunt clicant sobre la paraula formulari.",
+    titol: "ğŸ“„ Casal tecnificaciÃ³",
+    cos: "Benvolgudes famÃ­lies,\n\nUs informem que aquest estiu, a banda del casal d'estiu habitual, tambÃ© organitzarem un Casal de TecnificaciÃ³ durant el mes de juliol.\n\nAquest casal es realitzarÃ  de dilluns a dijous de 18h a 20h i estarÃ  orientat a oferir un treball tÃ¨cnic mÃ©s especÃ­fic i personalitzat, adaptat a les necessitats i evoluciÃ³ dels participants.\n\nLes setmanes de tecnificaciÃ³ estaran subjectes a un nombre mÃ­nim i mÃ xim d'inscripcions, ja que volem garantir una atenciÃ³ mÃ©s individualitzada i una millor qualitat del treball realitzat. Per aquest motiu, les places seran limitades.\n\nPer a mÃ©s informaciÃ³, podeu accedir al formulari adjunt clicant sobre la paraula formulari.",
     formulari: "https://forms.gle/DzweQnFUBMWbCVGu5",
-    formulariLabel: "Formulari d'inscripci¨®",
+    formulariLabel: "Formulari d'inscripciÃ³",
   },
   {
     id: 2,
     data: "2026-05-05",
-    titol: "?? Casal d'estiu CV Martorell",
-    cos: "Benvolgudes fam¨ªlies,\n\nUs informem que aquest estiu el club organitza el casal d'estiu del 29 de juny al 24 de juliol.\n\nPer con¨¨ixer el funcionament, activitats, horaris i fer la inscripci¨®, cal accedir al formulari adjunt.\n\nEl full d'inscripci¨® i el certificat de salut es poden descarregar des del mateix formulari i entregar-los a l'oficina o adjuntar-los on indica 'afegir arxiu'.\n\nPer qualsevol dubte, estem a la vostra disposici¨®.\n\nGr¨¤cies!\nJunta CV Martorell",
+    titol: "ğŸ“„ Casal d'estiu CV Martorell",
+    cos: "Benvolgudes famÃ­lies,\n\nUs informem que aquest estiu el club organitza el casal d'estiu del 29 de juny al 24 de juliol.\n\nPer conÃ¨ixer el funcionament, activitats, horaris i fer la inscripciÃ³, cal accedir al formulari adjunt.\n\nEl full d'inscripciÃ³ i el certificat de salut es poden descarregar des del mateix formulari i entregar-los a l'oficina o adjuntar-los on indica 'afegir arxiu'.\n\nPer qualsevol dubte, estem a la vostra disposiciÃ³.\n\nGrÃ cies!\nJunta CV Martorell",
     formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfxq82wpf1tF8NLqa2uTfMTJJUu6unsYV-WxdhwBN60SIfEvw/viewform",
-    formulariLabel: "Formulari d'inscripci¨®",
+    formulariLabel: "Formulari d'inscripciÃ³",
   },
   {
     id: 3,
     data: "2026-04-30",
-    titol: "?? Formulari de preinscripci¨® 26-27",
-    cos: "Us enviem el formulari de preinscripci¨® per a la temporada 26-27 (tamb¨¦ el rebreu per mail). Nom¨¦s cal omplir-lo una vegada.\n\nTeniu com a data l¨ªmit per enviar-ho el 30 de juny. Us agrairem que el feu dins del termini, o el m¨¦s aviat possible si ho teniu clar, per poder organitzar la temporada vinent.\n\nMoltes gr¨¤cies! ??",
+    titol: "ğŸ“„ Formulari de preinscripciÃ³ 26-27",
+    cos: "Us enviem el formulari de preinscripciÃ³ per a la temporada 26-27 (tambÃ© el rebreu per mail). NomÃ©s cal omplir-lo una vegada.\n\nTeniu com a data lÃ­mit per enviar-ho el 30 de juny. Us agrairem que el feu dins del termini, o el mÃ©s aviat possible si ho teniu clar, per poder organitzar la temporada vinent.\n\nMoltes grÃ cies! ğŸ™Œ",
     formulari: "https://docs.google.com/forms/d/e/1FAIpQLSfbgN7OpMD3cndujoOMAdnqMNSOo5G2oOqa1DuiVlTk9ZMfAw/viewform",
-    formulariLabel: "Formulari de preinscripci¨®",
+    formulariLabel: "Formulari de preinscripciÃ³",
   },
   {
     id: 4,
     data: "2026-04-29",
-    titol: "??? Noves samarretes d¡¯estiu del Club Volei Martorell!",
-    cos: "Ens fa molta il¡¤lusi¨® anunciar que ja estan a la venda! ??\n\nL¡¯objectiu ¨¦s omplir la grada de color, energia i suport als partits ???? Ens encantaria veure totes les fam¨ªlies animant juntes i lluint els colors del club!\n\n?? Com fer la comanda?\nOmpliu el formulari amb les vostres dades i talla.\n\n?? El pagament es far¨¤ presencialment al despatx n¨²m. 3 i en efectiu.\nEl preu de la samarreta ¨¦s de 12€.\n\n?? Si teniu dubtes amb la talla, podeu passar en horari d¡¯oficina:\n\n- Dilluns: 19h a 20:30h\n- Divendres: 19h a 21h\n\nHi ha diferents talles disponibles per a tothom!\n\nEl vostre suport ¨¦s clau per seguir creixent com a club i comunitat ??\n\nMoltes gr¨¤cies per formar-ne part! ??",
+    titol: "ğŸ›ï¸ Noves samarretes dâ€™estiu del Club Volei Martorell!",
+    cos: "Ens fa molta ilÂ·lusiÃ³ anunciar que ja estan a la venda! ğŸ™Œ\n\nLâ€™objectiu Ã©s omplir la grada de color, energia i suport als partits â¤ï¸ğŸ Ens encantaria veure totes les famÃ­lies animant juntes i lluint els colors del club!\n\nğŸ‘‰ Com fer la comanda?\nOmpliu el formulari amb les vostres dades i talla.\n\nğŸ’³ El pagament es farÃ  presencialment al despatx nÃºm. 3 i en efectiu.\nEl preu de la samarreta Ã©s de 12â‚¬.\n\nğŸ“ Si teniu dubtes amb la talla, podeu passar en horari dâ€™oficina:\n\n- Dilluns: 19h a 20:30h\n- Divendres: 19h a 21h\n\nHi ha diferents talles disponibles per a tothom!\n\nEl vostre suport Ã©s clau per seguir creixent com a club i comunitat ğŸ’ª\n\nMoltes grÃ cies per formar-ne part! ğŸ™",
     formulari: "https://forms.gle/MfYhvdHvXk2EnuEj8",
     formulariLabel: "Formulari samarretes",
   }
-  // Afegeix aqu¨ª els propers comunicats
+  // Afegeix aquÃ­ els propers comunicats
 ];
